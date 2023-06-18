e2b4eda3f4edc3d36a1c508b12ddee00701f5488
CaptainGlobe2 <farishaj64@gmail.com>
2023-06-18 10:29:15 +0530

    singleton class

---
+++ singleton.java
@@ -0,0 +1,28 @@
+class ton{
+    private static ton tonobj;
+
+    private ton(){
+
+    }
+    public static ton getInstant(){
+        if(tonobj==null){
+            tonobj= new ton();
+        }
+        return tonobj;
+    }
+    public void message(){
+        System.out.println("ton object is created");
+    }
+}
+
+public class singleton {
+    public static void main(String[] args){
+        ton t1;
+        t1=ton.getInstant();
+        ton t2;
+        t1.getInstant();
+        t2=ton.getInstant();
+        t1.message();
+        t2.message();
+    }
+}
