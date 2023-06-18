203fcf95d2c2f77acb0878bae200c13d626fd457
CaptainGlobe2 <farishaj64@gmail.com>
2023-06-18 10:49:06 +0530

    this code show how enum class working and automatic object creation and it always public static final by default and all

--- Enum.java
+++ Enum.java
@@ -31,3 +31,37 @@ public class Enum {
 //An enum class can include methods and fields just like regular classes.
 //When we create an enum class, the compiler will create instances (objects) of each enum constants
 // Also, all enum constant is always public static final by default.
+
+/*
+ * enum Size{
+  SMALL, MEDIUM, LARGE, EXTRALARGE;
+
+  public String getSize() {
+
+    // this will refer to the object SMALL
+    switch(this) {
+      case SMALL:
+        return "small";
+
+      case MEDIUM:
+        return "medium";
+
+      case LARGE:
+        return "large";
+
+      case EXTRALARGE:
+        return "extra large";
+
+      default:
+        return null;
+      }
+   }
+
+  public static void main(String[] args) {
+
+    // call getSize()
+    // using the object SMALL
+    System.out.println("The size of the pizza is " + Size.SMALL.getSize());
+  }
+}
+ */
