f3e417f82436479ab180f250d3aafb3d871aeae6
CaptainGlobe2 <farishaj64@gmail.com>
2023-06-19 12:26:02 +0530

    runnable interface using for thread to extends multiple classes and in the runnable interface there was run method in abstract

--- MultiThreadingdemo.java
+++ MultiThreadingdemo.java
@@ -1,4 +1,4 @@
-class database extends Thread {
+class database implements Runnable {//what happend if we want the anoter class to extend this class multiple inheritace is not possible so use the interface
 
     public void run(){//in the run method the thread start the excution 
         for(int i=0;i<5;i++){
@@ -29,11 +29,14 @@ class user extends Thread{//if we want to add the thread to the programm
 
 public class MultiThreadingdemo {
     public static void main(String [] args) throws InterruptedException{
-        database db=new database();
+        Runnable db=new database();//we use runnable for creating the reference of database because of the runnable is the superclass
         user u1=new user();
-        db.start();
+        Thread t1 =new Thread(db);//after passing the db to thread it get to know where to start and what method we are calling 
+        t1.start();//ater calling this method there was no relation between in the thread class to db
+        //db.start(); after using runnable its not working  because they does not define anything in the runnable interface about the run method they use abstract method for run so we have to create the thread class in the main method
         u1.start();
-        db.join();
+        t1.join();
+        //db.join();
         u1.join();//after using join the jvm cosider to complete the above thread
         System.out.println("last message");//but in the main method the thread start priniting this method at first because it consider the above both are programm of others and print this first if wee want to make it order use join
     }
