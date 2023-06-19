f8cc02c367ce28a90d64a0f17e44883e33e33254
CaptainGlobe2 <farishaj64@gmail.com>
2023-06-19 11:50:27 +0530

    in this code the display method is executed based on the flow  of the programmm if i want to run the next method simultaneously i have to create the thread of the programm

--- MultiThreadingdemo.java
+++ MultiThreadingdemo.java
@@ -1,14 +1,21 @@
 class database{
+
     public void display(){
         for(int i=0;i<5;i++){
             System.out.println("updating the db");
+             try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         }
     }
 }
 
 class user{
     public void print(){
-        for(int i=0;i<5;i++){
+        for(int i=1;i<=5;i++){
             System.out.println("user "+i);
         }
     }
@@ -18,5 +25,7 @@ public class MultiThreadingdemo {
     public static void main(String [] args){
         database db=new database();
         user u1=new user();
+        db.display();
+        u1.print();
     }
 }
