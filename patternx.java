a8db253b84cc70a1cd98d9123730ee7d25e72244
CaptainGlobe2 <farishaj64@gmail.com>
2023-06-22 11:37:10 +0530

    with user input

--- patternx.java
+++ patternx.java
@@ -1,10 +1,12 @@
+import java.util.Scanner;
+
 class oddletters{
     public void patternodd(String word){
         int length= word.length();
         System.out.println(length);
         for(int i=0;i<length;i++){
             for(int j=0;j<length;j++){
-                if(i==j||i+j==length-1){// this is the core of the programm the condition i give check the i and j is equal at ever iteration if i =0 if j=0 it print the p and check i+j=length-1 i+j=length-1 0+0=5-1 wrong 0+5=lenght-1
+                if(i==j||i+j==length-1){// this is the core of the programm the condition i give check the i and j is equal at ever iteration if i =0 if j=0 it print the p and check i+j=length-1 i+j=length-1 0+0=5-1 wrong 0+5==6-1 so it prints the last word of the programm
                     System.out.print(word.charAt(i));
                 }
                 else{
@@ -19,6 +21,9 @@ class oddletters{
 public class patternx {
     public static void main(String[] args){
         oddletters od =new oddletters();
-        od.patternodd("programmer");
+        System.out.println("enter the word ");
+        Scanner input = new Scanner(System.in);
+        String word = input.nextLine();
+        od.patternodd(word);
     }
 }
